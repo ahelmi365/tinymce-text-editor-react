23e875c2cbf76576a20371ba2aead74dc2eaf9ac
Ali Helmi <ahelmi365@gmail.com>
2024-05-20 15:00:57 +0300

    add css style to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
-
 import "./App.css";
+import TinyTextEditor from "./components/TinyTextEditor/TinyTextEditor";
+import PreviewHTMLEditorText from "./components/previewHTMLEditorText/PreviewHTMLEditorText";
 
 function App() {
-  return <></>;
+  const [editorState, setEditorState] = useState("");
+
+  return (
+    <div className="main">
+      <section className="text-editor-container">
+        <TinyTextEditor
+          setEditorState={setEditorState}
+          defaultEditorValue={editorState}
+          heightInPX={200}
+          placeholder={"Write somtheing here..."}
+        />
+      </section>
+
+      <section className="preview-text-editor-container card">
+        <PreviewHTMLEditorText value={editorState} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
