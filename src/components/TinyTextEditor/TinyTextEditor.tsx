78ace93145561d5e271452e9ec157fe19511d37c
Ali Helmi <ahelmi365@gmail.com>
2024-05-20 19:04:27 +0300

    update initial value

--- src/components/TinyTextEditor/TinyTextEditor.tsx
+++ src/components/TinyTextEditor/TinyTextEditor.tsx
@@ -54,6 +54,7 @@ const TinyTextEditor = ({
 
           content_style:
             "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+
           placeholder: placeholder,
         }}
         onEditorChange={setEditorState}
